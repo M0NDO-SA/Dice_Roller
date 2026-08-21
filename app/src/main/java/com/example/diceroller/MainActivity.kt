abab83ed372d7488d7d619d454bb9d6e28d67c0f
Gueye-Malick-Simon-Pierre <malickgueye2801@gmail.com>
2026-08-21 09:19:18 +0000

    Troisième commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,8 +30,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 import com.example.diceroller.ui.theme.DiceRollerTheme
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 
 
 class MainActivity : ComponentActivity() {
@@ -45,25 +47,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
 ) {
+
+    var result by remember { mutableStateOf(1) }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
+
         Image(
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription = "1"
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
         )
+
         Spacer(
             modifier = Modifier.height(16.dp)
         )
+
         Button(
             onClick = {
                 result = (1..6).random()
-
             }
         ) {
             Text(
@@ -73,9 +90,11 @@ fun DiceWithButtonAndImage(
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
+
     DiceWithButtonAndImage(
         modifier = Modifier
             .fillMaxSize()
